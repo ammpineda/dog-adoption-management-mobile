e829b1b65beca01fc8646578e2c8bb1f3bc4bd5b
ammpineda <pinedaandreamaym@gmail.com>
2024-02-04 03:19:19 +0800

    backend update 1

---
+++ backend/src/main/java/com/midrealm/backend/DogAdoptionAppApplication.java
@@ -0,0 +1,13 @@
+package com.midrealm.DogAdoptionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogAdoptionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogAdoptionAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/midrealm/DogAdoptionApp/DogAdoptionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midrealm.DogAdoptionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogAdoptionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
